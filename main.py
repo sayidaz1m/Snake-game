5737c1f0457fa852dc49b462d425c7d08982906f
Sayidazim <xamzayew@gmail.com>
2025-11-11 00:46:05 -0700

    init

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import pygame 
+from sys import exit 
 
 pygame.init()
-# screen creating
-screen = pygame.display.set_mode((800, 800))
+pygame.display.set_caption("SNAKE GAME")
+clock = pygame.time.Clock()
 
+test_surface = pygame.Surface((100, 200))
+test_surface.fill('blue')
+
+# screen creating
+screen = pygame.display.set_mode((860, 640))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(test_surface,(0,0))        
+
+    pygame.display.update()
+    clock.tick(60)
