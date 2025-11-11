aa08dbf1323d802539c3b51ce05c5714f22a5b1f
Sayidazim <xamzayew@gmail.com>
2025-11-10 23:13:17 -0700

    init

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
+import pygame 
 
-# main screen 
-import pygame, sys
 pygame.init()
-screen = pygame.display.set_mode((600, 600))
-clock = pygame.time.Clock()
-while True:
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            pygame.quit(); sys.exit()
-    screen.fill((20,20,20))
-    pygame.display.flip()
-    clock.tick(60)
-    #111
-    #222
-    #git 
+# screen creating
+screen = pygame.display.set_mode((800, 800))
+
